3657f61e3c4ee54da8cf4ff5555a2fdca9777425
Harsh Bhatt <hbhatt687@gmail.com>
2019-05-01 17:36:25 -0400

    checkout styling updated

--- checkout.php
+++ checkout.php
@@ -2,83 +2,85 @@
 require "header.php";
 ?>
 <!-- For all of the placeholder's, we can use the information from the dB. For now, its just test data-->
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-      
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="Lewis Henry">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="lhenry23@gsu.edu">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="Sample Address">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="Georgia">
-
+<div class="checkout-box">
+    <div class="row">
+    <div class="col-75">
+        <div class="container">
+        <form action="/action_page.php">
+        
             <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="GA">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>
-          </div>
+            <div class="col-50">
+                <h3>Billing Address</h3>
+                <label class="checkout-label" for="fname"><i class="fa fa-user"></i> Full Name</label>
+                <input class="checkout" type="text" id="fname" name="firstname" placeholder="Lewis Henry">
+                <label class="checkout-label" for="email"><i class="fa fa-envelope"></i> Email</label>
+                <input class="checkout" type="text" id="email" name="email" placeholder="lhenry23@gsu.edu">
+                <label class="checkout-label" for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                <input class="checkout" type="text" id="adr" name="address" placeholder="Sample Address">
+                <label class="checkout-label" for="city"><i class="fa fa-institution"></i> City</label>
+                <input class="checkout" type="text" id="city" name="city" placeholder="Georgia">
 
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
+                <div class="row">
+                <div class="col-50">
+                    <label class="checkout-label" for="state">State</label>
+                    <input class="checkout" type="text" id="state" name="state" placeholder="GA">
+                </div>
+                <div class="col-50">
+                    <label class="checkout-label" for="zip">Zip</label>
+                    <input class="checkout" type="text" id="zip" name="zip" placeholder="10001">
+                </div>
+                </div>
             </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="Place info from dB">
-            <!-- we need to add auto-complete feature here-->
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
+
+            <div class="col-50">
+                <h3>Payment</h3>
+                <label class="checkout-label" for="fname">Accepted Cards</label>
+                <div class="icon-container">
+                <i class="fa fa-cc-visa" style="color:navy;"></i>
+                <i class="fa fa-cc-amex" style="color:blue;"></i>
+                <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                <i class="fa fa-cc-discover" style="color:orange;"></i>
+                </div>
+                <label class="checkout-label" for="cname">Name on Card</label>
+                <input class="checkout" type="text" id="cname" name="cardname" placeholder="Place info from dB">
+                <!-- we need to add auto-complete feature here-->
+                <label class="checkout-label" for="ccnum">Credit card number</label>
+                <input class="checkout" type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+                <label class="checkout-label" for="expmonth">Exp Month</label>
+                <input class="checkout" type="text" id="expmonth" name="expmonth" placeholder="September">
+                <div class="row">
+                <div class="col-50">
+                    <label class="checkout-label" for="expyear">Exp Year</label>
+                    <input class="checkout" type="text" id="expyear" name="expyear" placeholder="2018">
+                </div>
+                <div class="col-50">
+                    <label class="checkout-label" for="cvv">CVV</label>
+                    <input class="checkout" type="text" id="cvv" name="cvv" placeholder="352">
+                </div>
+                </div>
             </div>
-          </div>
-          
+            
+            </div>
+            <label class="checkout-label" >
+            <input class="checkout" type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+            </label>
+            <input type="submit" value="Continue to checkout" class="btn">
+        </form>
         </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
     </div>
-  </div>
-  <div class="col-25">
-    <div class="container">
-        <!-- we need to display infromation from the database here, rn its just test data-->
-      <h4>Cart *DEMO, GET FORM DB*<span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
-      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
-      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
-      <hr>
-      <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+    <div class="col-25">
+        <div class="container">
+            <!-- we need to display infromation from the database here, rn its just test data-->
+        <h4>Cart *DEMO, GET FORM DB*<span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
+        <p><a href="#">Product 1</a> <span class="price">$15</span></p>
+        <p><a href="#">Product 2</a> <span class="price">$5</span></p>
+        <p><a href="#">Product 3</a> <span class="price">$8</span></p>
+        <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+        <hr>
+        <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+        </div>
     </div>
-  </div>
+    </div>
 </div>
 
 <?php
