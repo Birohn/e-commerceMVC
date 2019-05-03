3f8f32e3f6b18333615725556bebbce2b3c021d6
Harsh Bhatt <hbhatt687@gmail.com>
2019-05-02 21:40:21 -0400

    added auto-detect functionality to credit cards

--- footer.php
+++ footer.php
@@ -7,27 +7,22 @@
 
         //start without knowing the credit card type
         var result = "unknown";
-
         //first check for MasterCard
         if (/^5[1-5]/.test(accountNumber)) {
             result = "mastercard";
         }
-
         //then check for Visa
         else if (/^4/.test(accountNumber)) {
             result = "visa";
         }
-
         //then check for AmEx
         else if (/^3[47]/.test(accountNumber)) {
             result = "amex";
         }
-
         //then check for discover
         else if (/^6/.test(accountNumber)) {
             result = "discover";
         }
-
         return result;
     }
 
